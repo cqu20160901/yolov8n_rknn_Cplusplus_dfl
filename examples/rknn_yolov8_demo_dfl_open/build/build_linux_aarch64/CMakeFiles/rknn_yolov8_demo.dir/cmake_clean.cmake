1a3b5a51b75502a0c0fc45c558b380ed13a1ea02
zhngqn <1013429609@qq.com>
2024-01-11 11:59:15 +0800

    upload

---
+++ examples/rknn_yolov8_demo_dfl_open/build/build_linux_aarch64/CMakeFiles/rknn_yolov8_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rknn_yolov8_demo.dir/src/main.cc.o"
+  "CMakeFiles/rknn_yolov8_demo.dir/src/postprocess.cc.o"
+  "rknn_yolov8_demo"
+  "rknn_yolov8_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rknn_yolov8_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
